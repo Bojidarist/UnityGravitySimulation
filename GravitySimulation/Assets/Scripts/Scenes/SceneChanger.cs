209e861fbf45753a693464c90283dce3635188e0
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-10-15 13:17:16 +0300

    Add main menu
    MainScene is now the main menu.
    OrbiterExample and AttractorExample are in different scenes.
    AttractorExample is blank.

---
+++ GravitySimulation/Assets/Scripts/Scenes/SceneChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(ScenesStruct.MainMenu);
+    }
+
+    public void AttractorExample()
+    {
+        SceneManager.LoadScene(ScenesStruct.AttractorExample);
+    }
+
+    public void OrbiterExample()
+    {
+        SceneManager.LoadScene(ScenesStruct.OrbiterExample);
+    }
+}
---
+++ GravitySimulation/Assets/Scripts/Scenes/ScenesStruct.cs
@@ -0,0 +1,6 @@
+﻿public struct ScenesStruct
+{
+    public static string MainMenu = "MainScene";
+    public static string AttractorExample = "AttractorExample";
+    public static string OrbiterExample = "OrbiterExample";
+}
